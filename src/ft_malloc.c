2d09d31f79e0098a424026ffdd3b4cb777999f05
Vallium <anton.alliot@gmail.com>
2017-03-31 18:02:08 +0200

    First test with mmap for allocation

--- src/ft_malloc.c
+++ src/ft_malloc.c
@@ -6,16 +6,17 @@
 /*   By: aalliot <aalliot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/01 16:51:55 by aalliot           #+#    #+#             */
-/*   Updated: 2017/03/29 18:02:51 by aalliot          ###   ########.fr       */
+/*   Updated: 2017/03/31 16:36:18 by aalliot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft_malloc.h>
+#include <sys/mman.h>
 
 void	*malloc(size_t size)
 {
-	(void)size;
+	void	*ret;
 
-	ft_putchar('b');
-	return (NULL);
+	ret = mmap(0, size, PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
+	return (ret);
 }
