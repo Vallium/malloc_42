b0b538655b9ab26628b1f8d74ba375aed38257e7
Vallium <anton.alliot@gmail.com>
2017-07-26 17:58:43 +0200

    [~] norme on malloc OK

--- src/malloc.c
+++ src/malloc.c
@@ -6,13 +6,13 @@
 /*   By: aalliot <aalliot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/01 16:51:55 by aalliot           #+#    #+#             */
-/*   Updated: 2017/07/26 17:27:26 by aalliot          ###   ########.fr       */
+/*   Updated: 2017/07/26 17:58:17 by aalliot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft_malloc.h"
 
-t_allocs g_allocs = { 
+t_allocs g_allocs = {
 	.nb_zones = 0,
 	.zones = NULL
 };
@@ -21,7 +21,7 @@ void	*malloc(size_t size)
 {
 	void	*ret;
 
-	if (size <= TINY_MAX_SIZE)	
+	if (size <= TINY_MAX_SIZE)
 		ret = new_alloc(size, TINY);
 	else if (size <= SMALL_MAX_SIZE)
 		ret = new_alloc(size, SMALL);
